fa78e35c146c78b2f68aa3b0b474cd05f2bf2ae5
DarKRs <centralstreet_24@mail.ru>
2018-09-13 11:41:55 +0600

    Общение
    Примитивное общение

--- WhatMovieToday/Main.Designer.cs
+++ WhatMovieToday/Main.Designer.cs
@@ -34,14 +34,12 @@
             this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.авторToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.GenreList = new System.Windows.Forms.RichTextBox();
-            this.FilmList = new System.Windows.Forms.RichTextBox();
-            this.ChatBox = new System.Windows.Forms.RichTextBox();
             this.помощьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.стандартныеКомандыБотаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FilmList = new System.Windows.Forms.RichTextBox();
+            this.GenreList = new System.Windows.Forms.RichTextBox();
+            this.ChatBox = new System.Windows.Forms.RichTextBox();
             this.UpperMenu.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // UpperMenu
@@ -83,42 +81,6 @@
             this.выходToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
             this.выходToolStripMenuItem.Text = "Выход";
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.ChatBox);
-            this.panel1.Controls.Add(this.GenreList);
-            this.panel1.Controls.Add(this.FilmList);
-            this.panel1.Location = new System.Drawing.Point(12, 27);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(972, 331);
-            this.panel1.TabIndex = 1;
-            // 
-            // GenreList
-            // 
-            this.GenreList.Location = new System.Drawing.Point(3, 3);
-            this.GenreList.Name = "GenreList";
-            this.GenreList.ReadOnly = true;
-            this.GenreList.Size = new System.Drawing.Size(389, 199);
-            this.GenreList.TabIndex = 1;
-            this.GenreList.Text = "";
-            // 
-            // FilmList
-            // 
-            this.FilmList.Location = new System.Drawing.Point(398, 3);
-            this.FilmList.Name = "FilmList";
-            this.FilmList.ReadOnly = true;
-            this.FilmList.Size = new System.Drawing.Size(571, 199);
-            this.FilmList.TabIndex = 0;
-            this.FilmList.Text = "";
-            // 
-            // ChatBox
-            // 
-            this.ChatBox.Location = new System.Drawing.Point(3, 208);
-            this.ChatBox.Name = "ChatBox";
-            this.ChatBox.Size = new System.Drawing.Size(966, 120);
-            this.ChatBox.TabIndex = 2;
-            this.ChatBox.Text = "";
-            // 
             // помощьToolStripMenuItem
             // 
             this.помощьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -133,20 +95,51 @@
             this.стандартныеКомандыБотаToolStripMenuItem.Size = new System.Drawing.Size(227, 22);
             this.стандартныеКомандыБотаToolStripMenuItem.Text = "Стандартные команды бота";
             // 
+            // FilmList
+            // 
+            this.FilmList.Location = new System.Drawing.Point(18, 27);
+            this.FilmList.Name = "FilmList";
+            this.FilmList.ReadOnly = true;
+            this.FilmList.Size = new System.Drawing.Size(571, 199);
+            this.FilmList.TabIndex = 0;
+            this.FilmList.Text = "";
+            // 
+            // GenreList
+            // 
+            this.GenreList.Location = new System.Drawing.Point(595, 27);
+            this.GenreList.Name = "GenreList";
+            this.GenreList.ReadOnly = true;
+            this.GenreList.Size = new System.Drawing.Size(389, 199);
+            this.GenreList.TabIndex = 1;
+            this.GenreList.Text = "";
+            // 
+            // ChatBox
+            // 
+            this.ChatBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ChatBox.Location = new System.Drawing.Point(18, 232);
+            this.ChatBox.Name = "ChatBox";
+            this.ChatBox.Size = new System.Drawing.Size(966, 120);
+            this.ChatBox.TabIndex = 2;
+            this.ChatBox.Text = "";
+            this.ChatBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ChatBox_KeyPress);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(996, 370);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ChatBox);
+            this.Controls.Add(this.GenreList);
             this.Controls.Add(this.UpperMenu);
+            this.Controls.Add(this.FilmList);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
             this.MainMenuStrip = this.UpperMenu;
             this.Name = "Main";
             this.Text = "WhatMovieToday?";
             this.UpperMenu.ResumeLayout(false);
             this.UpperMenu.PerformLayout();
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -159,11 +152,10 @@
         private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem авторToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.RichTextBox GenreList;
-        private System.Windows.Forms.RichTextBox FilmList;
         private System.Windows.Forms.ToolStripMenuItem помощьToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem стандартныеКомандыБотаToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox FilmList;
+        private System.Windows.Forms.RichTextBox GenreList;
         private System.Windows.Forms.RichTextBox ChatBox;
     }
 }
--- WhatMovieToday/Main.cs
+++ WhatMovieToday/Main.cs
@@ -7,16 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace WhatMovieToday
 {
     public partial class Main : Form
     {
+        Random rnd = new Random();
         public Main()
         {
             InitializeComponent();
             AnswerBase.ParserFromTxt.Parse();
+
+            ChatBox.Text += "Доброго времени суток. Я бот который поможет вам подобрать фильм. Чтобы начать общение напишите что-либо. \n\n";
         }
 
+        private void ChatBox_KeyPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (ChatBox.Lines[ChatBox.Lines.Length - 1] == "") { return; }
+                //MessageBox.Show("Нажали <ENTER>");
+                
+                Thread.Sleep(rnd.Next(100,500));
+                ChatBox.Text += "\n" + AnswerBase.ParserFromTxt.GenerateAnswer(ChatBox.Lines[ChatBox.Lines.Length-1]) + "\n";
+                ChatBox.SelectionStart = ChatBox.Text.Length;
+            }
+        }
     }
 }
